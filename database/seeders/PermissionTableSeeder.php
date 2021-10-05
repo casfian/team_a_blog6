1ea31fba6d948ac2207cbb1565a58a0ca6851abc
BB Caspian <casfian@gmail.com>
2021-10-05 15:05:27 +0800

    seeder

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Permission; //add this
 
 class PermissionTableSeeder extends Seeder
 {
