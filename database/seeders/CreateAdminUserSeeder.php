29a1406a7cbde769631fea79f4d74371313ee593
BB Caspian <casfian@gmail.com>
2021-10-05 15:54:52 +0800

    added seeder for Admin

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\user; //add this
+use Spatie\Permission\Models\Role; //add this
+use Spatie\Permission\Models\Permission; //add this
 
 class CreateAdminUserSeeder extends Seeder
 {
@@ -13,6 +16,23 @@ class CreateAdminUserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::create([
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('12345678'),
+            ]);
+
+        $role = Role::create([
+            'name' => 'Admin'
+        ]);
+
+        $permissions = Permission::pluck('id', 'id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([
+            $role->id
+        ]);
+
     }
 }
