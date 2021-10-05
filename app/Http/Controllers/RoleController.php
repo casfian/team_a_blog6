0cf6cc4334b2c82ac495e698555fa8fea4053d92
BB Caspian <casfian@gmail.com>
2021-10-05 15:47:00 +0800

    create construct at rolecontroller

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -13,6 +13,15 @@ class RoleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    function __construct()
+    {
+        $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index', 'store']]);
+        $this->middleware('permission:role-create', ['only' => ['create', 'store']]);
+        $this->middleware('permission:role-edit', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:role-delete', ['only' => ['destroy']]);
+    }
+
     public function index(Request $request)
     {
         $roles = Role::orderby('id', 'DESC')->paginate(8);
